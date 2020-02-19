5dadc95e135470df6a91b80a0424f346d548f936
Krystal <kaism@pm.me>
2020-02-19 14:48:40 +0100

    Add search

--- app/src/main/java/com/example/books/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/books/ui/search/SearchFragment.java
@@ -1,22 +1,53 @@
 package com.example.books.ui.search;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import com.example.books.R;
 
 public class SearchFragment extends Fragment {
 
+	private Context context;
+	private SearchView searchView;
+
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View root = inflater.inflate(R.layout.fragment_search, container, false);
-		return root;
+		View view = inflater.inflate(R.layout.fragment_search, container, false);
+		searchView = view.findViewById(R.id.search);
+		return view;
+	}
+
+	@Override
+	public void onAttach(@NonNull Context context) {
+		super.onAttach(context);
+		this.context = context;
+	}
+
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+			@Override
+			public boolean onQueryTextSubmit(String query) {
+				Toast.makeText(context, "Searching for " + query, Toast.LENGTH_LONG).show();
+				return false;
+			}
+
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				return false;
+			}
+		});
+
 	}
 
 }

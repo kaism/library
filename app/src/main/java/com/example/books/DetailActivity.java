544fe25b36fa33797f1063ff31cb1bba9bfe97d3
Krystal <kaism@pm.me>
2020-02-20 17:53:20 +0100

    Add up button on DetailActivity
    Delete unnecessary parent declarations in manifest

--- app/src/main/java/com/example/books/DetailActivity.java
+++ app/src/main/java/com/example/books/DetailActivity.java
@@ -1,8 +1,12 @@
 package com.example.books;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,11 +18,22 @@ public class DetailActivity extends AppCompatActivity {
 	TextView titleView;
 	ImageView imageView;
 	TextView descriptionView;
+	ActionBar actionBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail);
+
+		// my_child_toolbar is defined in the layout file
+		Toolbar myChildToolbar = findViewById(R.id.my_child_toolbar);
+		setSupportActionBar(myChildToolbar);
+
+		// set ActionBar and enable up button
+		actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setDisplayHomeAsUpEnabled(true);
+		}
 
 		// initialize the views
 		titleView = findViewById(R.id.list_item_title_detail);
@@ -29,5 +44,15 @@ public class DetailActivity extends AppCompatActivity {
 		titleView.setText(getIntent().getStringExtra("title"));
 		Picasso.get().load(getIntent().getStringExtra("image")).into(imageView);
 		descriptionView.setText(getIntent().getStringExtra("description"));
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		super.onOptionsItemSelected(item);
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+		}
+
+		return true;
 	}
 }
